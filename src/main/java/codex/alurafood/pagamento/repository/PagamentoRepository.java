a8727ece15cec0bb4d314b0f57925bb6171d2ced
Matheuszy <matheusalmeidacarlos@gmail.com>
2026-06-08 15:41:47 -0300

    feat: annotate PagamentoRepository with @Repository

--- src/main/java/codex/alurafood/pagamento/repository/PagamentoRepository.java
+++ src/main/java/codex/alurafood/pagamento/repository/PagamentoRepository.java
@@ -2,6 +2,8 @@ package codex.alurafood.pagamento.repository;
 
 import codex.alurafood.pagamento.model.Pagamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
 }
