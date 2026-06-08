082e69fcb107af176891e7fb37d968687d1a0164
Matheuszy <matheusalmeidacarlos@gmail.com>
2026-06-08 16:48:18 -0300

    feat: include payment ID in PagamentoResponse for listing and retrieval

--- src/main/java/codex/alurafood/pagamento/dto/response/PagamentoResponse.java
+++ src/main/java/codex/alurafood/pagamento/dto/response/PagamentoResponse.java
@@ -5,6 +5,7 @@ import codex.alurafood.pagamento.model.Status;
 import java.math.BigDecimal;
 
 public record PagamentoResponse(
+        Long id,
         BigDecimal valor,
         Status status
 ) {
