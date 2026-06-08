b7e2555a4c97c80ac2733f937075070eda046ea9
Matheuszy <matheusalmeidacarlos@gmail.com>
2026-06-08 16:25:36 -0300

    feat: add PagamentoController for payment handling

---
+++ src/main/java/codex/alurafood/pagamento/controller/PagamentoController.java
@@ -0,0 +1,10 @@
+package codex.alurafood.pagamento.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("pagamento")
+public class PagamentoController {
+
+}
