dcb68451b69b6ad5f6b07e31cf35be8552d1fb96
Matheuszy <matheusalmeidacarlos@gmail.com>
2026-06-08 16:20:48 -0300

    feat: add methods to update and delete payment status in PagamentoService

--- src/main/java/codex/alurafood/pagamento/service/PagamentoService.java
+++ src/main/java/codex/alurafood/pagamento/service/PagamentoService.java
@@ -3,9 +3,11 @@ package codex.alurafood.pagamento.service;
 import codex.alurafood.pagamento.dto.request.PagamentoRequest;
 import codex.alurafood.pagamento.dto.response.PagamentoResponse;
 import codex.alurafood.pagamento.model.Pagamento;
+import codex.alurafood.pagamento.model.Status;
 import codex.alurafood.pagamento.repository.PagamentoRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -45,5 +47,41 @@ public class PagamentoService {
                         pagamento.getStatus()))
                 .getBody();
 
+    }
+
+    public PagamentoResponse atualizarStatusPagamento(Long id, PagamentoRequest request) {
+        Pagamento pagamento = pagamentoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Pagamento não encontrado"));
+
+        if (pagamento.getStatus() == Status.APROVADO) {
+            throw new RuntimeException("Não é possível alterar o status de um pagamento aprovado");
+        }
+        else if (pagamento.getStatus() == Status.CRIADO) {
+            pagamento.setValor(request.valor());
+            pagamento.setNome(request.nome());
+            pagamento.setNumero(request.numero());
+            pagamento.setExpiracao(request.expiracao());
+            pagamento.setStatus(request.status());
+            pagamento.setPedidoId(request.pedidoId());
+            pagamento.setFormaDePagamento(request.formaDePagamento());
+
+            pagamentoRepository.save(pagamento);
+            return ResponseEntity.status(HttpStatus.ACCEPTED)
+                    .body(new PagamentoResponse(pagamento.getValor(),
+                            pagamento.getStatus()))
+                    .getBody();
+        }
+
+        else {
+            throw new RuntimeException("Pagamento foi reprovado e não pode ser alterado");
+        }
+    }
+
+    public void deletarPagamento(Long id) {
+        if (pagamentoRepository.existsById(id)) {
+            pagamentoRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("Pagamento não encontrado");
+        }
     }
 }
